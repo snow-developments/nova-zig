45b327b4adbbff21d2edc2afd54e2b9cc6358b1e
Chance Snow <git@chancesnow.me>
2022-05-01 17:27:24 -0500

    Implement baseline syntax

---
+++ tests/test.zig
@@ -0,0 +1,32 @@
+// Foobar
+const foo = "";
+var bar = "";
+
+const Point = struct {
+    x: f32,
+    y: f32,
+};
+
+const Point_2 = packed struct {
+    x: f32,
+    y: f32,
+};
+
+const Payload = union {
+    int: i64,
+    float: f64,
+    boolean: bool,
+};
+
+const Flags = enum {};
+const NodeType = enum(u32) {};
+
+/// Do the thing
+fn foo() {
+  const bar = "";
+  print("foobar");
+}
+
+test "foobar" {
+  
+}
